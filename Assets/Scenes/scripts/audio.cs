bd94c6866ed5a46f60c61977768d573f4a050659
radish1014 <taiya1014@naver.com>
2019-06-16 19:56:16 +0900

    수정본

--- Assets/Scenes/scripts/audio.cs
+++ Assets/Scenes/scripts/audio.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mysql : MonoBehaviour
+
+public class audio : MonoBehaviour
 {
+    AudioSource one;
     // Start is called before the first frame update
     void Start()
     {
+        one = GetComponent<AudioSource>();
+
+        if (one.isPlaying)
+        {
+            one.Stop();
+        }
+        one.Play();
         
     }
 
